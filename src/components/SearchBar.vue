ab717b7952c8e98c6fd72e0c2c16702215e11405
Ayuba Olanrewaju <Babarindeolanrewaju@gmail.com>
2020-10-09 09:58:26 +0100

    first commit

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <input @input="onInput" />
+</div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    methods: {
+        onInput: function (event) {
+            // eslint-disable-next-line vue/custom-event-name-casing
+            this.$emit('termChange', event.target.value);
+        }
+    }
+};
+</script>
+
+<style scoped>
+input {
+    width: 75%;
+}
+
+div {
+    text-align: center;
+    margin: 20px;
+}
+</style>
